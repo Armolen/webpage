172bb34c7db93dc9e9686ca6fef09366ca5e405a
Armolen <benki07@gmail.com>
2017-11-03 17:16:01 +0100

    Second

--- strefa1.php
+++ strefa1.php
@@ -1,5 +1,5 @@
 
-    <div id="srodkowy" class="safe">
+    <div id="srodkowy" class="safe">UPDATE
         <div id="centered"><div id="rolling"><img src='images/details/ball.gif' /></div></div>
         </div>
  
